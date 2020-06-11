f6774255561e69d8660ab675c6f18e52fd9be859
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-06-11 14:15:18 +0200

    Improved Pagination component

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,64 +1,32 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
+import './pagination.css';
 
 export default class Pagination extends React.Component {
     render() {
-        const startPage = this.props.startEnd ? <a href="#" className={`chi-button -icon -flat ${this.props.inverse ? '-inverse' : ''}`} aria-label="Start page">
-            <div className="chi-button__content">
-                <i className="chi-icon icon-page-first" aria-hidden="true"></i>
-            </div>
-        </a> : null;
-        const lastPage = this.props.startEnd ? <a href="#" className={`chi-button -icon -flat ${this.props.inverse ? '-inverse' : ''}`} aria-label="Start page">
-            <div className="chi-button__content">
-                <i className="chi-icon icon-page-last" aria-hidden="true"></i>
-            </div>
-        </a> : null;
-        const pages = [];
-        const pagesToShowHalf = this.props.pagesToShow % 2 === 0 ? this.props.pagesToShow / 2 : (this.props.pagesToShow - 1) / 2;
-
-        for (let i = 0; i < this.props.pages; i++) {
-            pages.push(<a
-                href="#"
-                className={`
-                    chi-button -flat
-                    ${this.props.inverse ? '-light' : ''}
-                    ${i+1 === this.props.currentPage ? '-active' : ''}
-                `}>{i+1}</a>);
-        }
-
-        return <nav
-            className={`
-                chi-pagination
-                ${this.props.inverse ? '-inverse' : ''}
-            `}
-            role="navigation"
-            aria-label="Pagination">
-            <div className="chi-button-group">
-                {startPage}
-                <a href="#" className={`chi-button -icon -flat ${this.props.inverse ? '-light' : ''}`} aria-label="Previous">
-                    <div className="chi-button__content">
-                        <i className="chi-icon icon-chevron-left" aria-hidden="true"></i>
-                    </div>
-                </a>
-                {pages}
-                <a href="#" className={`chi-button -icon -flat ${this.props.inverse ? '-light' : ''}`}
-                   aria-label="Next">
-                    <div className="chi-button__content">
-                        <i className="chi-icon icon-chevron-right" aria-hidden="true"></i>
-                    </div>
-                </a>
-                {lastPage}
-            </div>
-        </nav>;
+        return  <chi-pagination
+            inverse={this.props.inverse}
+            pages={this.props.pages}
+            first-last={this.props.startEnd}
+            current-page={this.props.currentPage}
+            compact={this.props.compact}
+            page-size={this.props.pageSize}
+            page-jumper={this.props.pageJumper}
+            results={this.props.results}
+        ></chi-pagination>
     }
 }
 
 /* eslint-disable sort-keys */
 Pagination.propTypes = {
+    compact: PropTypes.bool,
     inverse: PropTypes.bool,
     pages: PropTypes.number,
     startEnd: PropTypes.bool,
     currentPage: PropTypes.number,
+    pageSize: PropTypes.bool,
+    pageJumper: PropTypes.bool,
+    results: PropTypes.number
 };
 /* eslint-enable sort-keys */
 
