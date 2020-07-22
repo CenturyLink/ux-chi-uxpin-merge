01c3d15ace8e713eab724aed4b32d98c163a1238
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-07-22 13:30:40 +0200

    Added support of Interactions to File Input component

--- src/components/forms/file-input/file-input.js
+++ src/components/forms/file-input/file-input.js
@@ -5,8 +5,19 @@ import { uuid4 } from '../../../utils/utils';
 const uuid = uuid4();
 /* eslint-disable */
 const FileInput = (props) => (
-  <div className="chi-form__item">
-    <input type="file" className={`chi-input -${props.size}`} id={uuid} aria-label="Choose file" />
+  <div className="chi-form__item"
+       onMouseEnter={props.mouseOver}
+       onMouseLeave={props.mouseLeave}
+       onMouseDown={props.mouseDown}
+       onMouseUp={props.mouseUp}>
+    <input
+      id={uuid}
+      type="file"
+      className={`chi-input -${props.size}`}
+      aria-label="Choose file"
+      onClick={props.click}
+      onFocus={props.focus}
+      onBlur={props.focusLost} />
     <label htmlFor={uuid}>{props.label}</label>
   </div>
   );
@@ -14,6 +25,13 @@ const FileInput = (props) => (
 FileInput.propTypes = {
   label: PropTypes.string,
   size: PropTypes.oneOf(['sm', 'md', 'lg', 'xl']),
+  click: PropTypes.func,
+  focus: PropTypes.func,
+  focusLost: PropTypes.func,
+  mouseDown: PropTypes.func,
+  mouseUp: PropTypes.func,
+  mouseOver: PropTypes.func,
+  mouseLeave: PropTypes.func,
 };
 /* eslint-enable */
 
