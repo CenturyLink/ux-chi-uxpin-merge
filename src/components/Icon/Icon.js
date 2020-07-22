b73ac49a43b9c76a167a96746f742929e89e62e4
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-07-22 15:13:12 +0200

    Added support of Interactions to Icon component

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,22 +1,32 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 
+/* eslint-disable */
 const Icon = (props) => (
   <i className={`
     chi-icon
     icon-${props.icon}
     ${props.color ? `-text--${props.color}` : ''}
     ${props.size ? `-${props.size}` : ''}
-  `}>
+  `}
+     onClick={props.click}
+     onMouseEnter={props.mouseOver}
+     onMouseLeave={props.mouseLeave}
+     onMouseDown={props.mouseDown}
+     onMouseUp={props.mouseUp}>
   </i>
 );
 
-/* eslint-disable */
 Icon.propTypes = {
   onClick: PropTypes.func,
   color: PropTypes.oneOf(['primary', 'secondary', 'dark', 'light', 'danger', 'grey', 'muted']),
   size: PropTypes.oneOf(['xs', 'sm', 'sm--2', 'sm--3', 'md', 'lg', 'xl', 'xxl']),
   icon: PropTypes.string,
+  click: PropTypes.func,
+  mouseDown: PropTypes.func,
+  mouseUp: PropTypes.func,
+  mouseOver: PropTypes.func,
+  mouseLeave: PropTypes.func,
 };
 /* eslint-enable */
 
