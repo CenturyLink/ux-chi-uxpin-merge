9c52648c741bd10dd50b1355195265c45397b5b1
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-07-22 12:19:28 +0200

    Added support of interactions to Number Input component

--- src/components/forms/number-input/number-input.js
+++ src/components/forms/number-input/number-input.js
@@ -1,8 +1,34 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 import Label from '../label/label';
+import { uuid4 } from '../../../utils/utils';
 
+const uuid = uuid4();
 export default class NumberInput extends React.Component {
+  componentDidMount() {
+    const numberInput = document.getElementById(`${uuid}`);
+
+    numberInput.addEventListener('chiChange', () => {
+      this.props.valueChange();
+    });
+    setTimeout(() => {
+      const input = numberInput.querySelector('input');
+      const self = this;
+
+      if (input) {
+        input.addEventListener('focus', () => {
+          self.props.focus();
+        });
+        input.addEventListener('blur', () => {
+          self.props.focusLost();
+        });
+        input.addEventListener('input', () => {
+          self.props.input();
+        });
+      }
+    }, 1000);
+  }
+
   render() {
     const label = this.props.label
       ? (
@@ -22,16 +48,21 @@ export default class NumberInput extends React.Component {
       <div className="chi-form__item">
         {label}
         <chi-number-input
+          id={uuid}
           disabled={this.props.disabled}
           expanded={this.props.expanded}
-          size={size}>
+          size={size}
+          onMouseEnter={this.props.mouseOver}
+          onMouseLeave={this.props.mouseLeave}
+          onMouseDown={this.props.mouseDown}
+          onMouseUp={this.props.mouseUp}>
         </chi-number-input>
       </div>
     );
   }
 }
 
-/* eslint-disable sort-keys */
+/* eslint-disable */
 NumberInput.propTypes = {
   disabled: PropTypes.bool,
   expanded: PropTypes.bool,
@@ -39,8 +70,16 @@ NumberInput.propTypes = {
   labelRequired: PropTypes.bool,
   labelOptional: PropTypes.bool,
   size: PropTypes.oneOf(['sm (24px)', 'md (32px)', 'lg (40px)', 'xl (48px)']),
+  focus: PropTypes.func,
+  focusLost: PropTypes.func,
+  input: PropTypes.func,
+  valueChange: PropTypes.func,
+  mouseDown: PropTypes.func,
+  mouseUp: PropTypes.func,
+  mouseOver: PropTypes.func,
+  mouseLeave: PropTypes.func,
 };
-/* eslint-enable sort-keys */
+/* eslint-enable */
 
 NumberInput.defaultProps = {
   disabled: false,
