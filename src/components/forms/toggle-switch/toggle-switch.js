8721c36e1d0b42bdf3a7026d46b18602fb798959
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-07-22 13:58:24 +0200

    Added support of interactions to Toggle Switch component

--- src/components/forms/toggle-switch/toggle-switch.js
+++ src/components/forms/toggle-switch/toggle-switch.js
@@ -3,9 +3,19 @@ import * as React from 'react';
 
 /* eslint-disable */
 const ToggleSwitch = (props) => (
-  <div className="chi-form__item">
+  <div className="chi-form__item"
+       onMouseEnter={props.mouseOver}
+       onMouseLeave={props.mouseLeave}
+       onMouseDown={props.mouseDown}
+       onMouseUp={props.mouseUp}>
     <label className="chi-switch">
-      <input type="checkbox" className="chi-switch__input" disabled={props.disabled} />
+      <input
+          type="checkbox"
+          className="chi-switch__input"
+          disabled={props.disabled}
+          onClick={props.click}
+          onFocus={props.focus}
+          onBlur={props.focusLost} />
       <span className="chi-switch__content">
         <span className="chi-switch__thumb"></span>
       </span>
@@ -17,6 +27,13 @@ const ToggleSwitch = (props) => (
 ToggleSwitch.propTypes = {
   disabled: PropTypes.bool,
   label: PropTypes.string,
+  click: PropTypes.func,
+  focus: PropTypes.func,
+  focusLost: PropTypes.func,
+  mouseDown: PropTypes.func,
+  mouseUp: PropTypes.func,
+  mouseOver: PropTypes.func,
+  mouseLeave: PropTypes.func,
 };
 /* eslint-enable */
 
