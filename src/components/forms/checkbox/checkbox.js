98dc3171bf7f86dc7ae82704f853509a532836b8
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-07-24 14:41:43 +0200

    Added support of individual interactions to checkboxes

--- src/components/forms/checkbox/checkbox.js
+++ src/components/forms/checkbox/checkbox.js
@@ -8,16 +8,35 @@ import * as React from 'react';
 export default class Checkbox extends React.Component {
   render() {
     const checkboxesToRender = [];
+    const toggleCheckbox = (checkboxLabel, i) => {
+      const input = checkboxLabel.parentNode.querySelector('input');
+
+      if (input) {
+        if (input.checked) {
+          input.checked = false;
+          if (this.props[`deselect${i}`]) {
+            this.props[`deselect${i}`]();
+          }
+        } else {
+          input.checked = true;
+          if (this.props[`select${i}`]) {
+            this.props[`select${i}`]();
+          }
+        }
+      }
+    };
 
     Array(11).fill()
         .forEach((_, i) => {
           if (this.props[`label${i}`]) {
             checkboxesToRender.push(
-                <div className={`chi-form__item ${this.props.inline ? '-inline' : ''}`}>
+                <div className={`chi-form__item ${this.props.inline ? '-inline' : ''}`} key={`checkbox-${i}`}>
                   <div className="chi-checkbox">
                     <input type="checkbox" className="chi-checkbox__input" disabled={this.props[`disabled${i}`]}
-                           checked={this.props[`checked${i}`]}/>
-                    <label className="chi-checkbox__label" htmlFor="checkbox1">{this.props[`label${i}`]}</label>
+                     checked={this.props[`checked${i}`]} onChange={(e) => {}} />
+                    <label onClick={(e) => {
+                      toggleCheckbox(e.target, i);
+                    }} className="chi-checkbox__label" htmlFor="checkbox1">{this.props[`label${i}`]}</label>
                   </div>
                 </div>
             );
@@ -39,33 +58,53 @@ Checkbox.propTypes = {
   label1: PropTypes.string,
   disabled1: PropTypes.bool,
   checked1: PropTypes.bool,
+  select1: PropTypes.func,
+  deselect1: PropTypes.func,
   label2: PropTypes.string,
   disabled2: PropTypes.bool,
   checked2: PropTypes.bool,
+  select2: PropTypes.func,
+  deselect2: PropTypes.func,
   label3: PropTypes.string,
   disabled3: PropTypes.bool,
+  select3: PropTypes.func,
+  deselect3: PropTypes.func,
   checked3: PropTypes.bool,
   label4: PropTypes.string,
   disabled4: PropTypes.bool,
   checked4: PropTypes.bool,
+  select4: PropTypes.func,
+  deselect4: PropTypes.func,
   label5: PropTypes.string,
   disabled5: PropTypes.bool,
   checked5: PropTypes.bool,
+  select5: PropTypes.func,
+  deselect5: PropTypes.func,
   label6: PropTypes.string,
   disabled6: PropTypes.bool,
   checked6: PropTypes.bool,
+  select6: PropTypes.func,
+  deselect6: PropTypes.func,
   label7: PropTypes.string,
   disabled7: PropTypes.bool,
   checked7: PropTypes.bool,
+  select7: PropTypes.func,
+  deselect7: PropTypes.func,
   label8: PropTypes.string,
   disabled8: PropTypes.bool,
   checked8: PropTypes.bool,
+  select8: PropTypes.func,
+  deselect8: PropTypes.func,
   label9: PropTypes.string,
   disabled9: PropTypes.bool,
   checked9: PropTypes.bool,
+  select9: PropTypes.func,
+  deselect9: PropTypes.func,
   label10: PropTypes.string,
   disabled10: PropTypes.bool,
-  checked10: PropTypes.bool
+  checked10: PropTypes.bool,
+  select10: PropTypes.func,
+  deselect11: PropTypes.func,
 };
 /* eslint-enable */
 
