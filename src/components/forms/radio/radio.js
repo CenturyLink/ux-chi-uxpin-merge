9c5d71f2b156a81479f8e1b3e0b164010b13eb28
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-07-24 13:53:52 +0200

    Added individual interactions to radios

--- src/components/forms/radio/radio.js
+++ src/components/forms/radio/radio.js
@@ -10,6 +10,19 @@ export default class Radio extends React.Component {
   render() {
     const radiosToRender = [];
 
+    function selectRadio(radioLabel) {
+      const input = radioLabel.parentNode.querySelector('input');
+      const otherInputsInFieldset = radioLabel.parentNode.parentNode.parentNode.querySelectorAll('input');
+
+      otherInputsInFieldset.forEach(input => {
+        input.checked = false;
+      });
+
+      if (input) {
+        input.checked = true;
+      }
+    }
+
     Array(11).fill()
         .forEach((_, i) => {
           if (this.props[`option${i}`]) {
@@ -17,11 +30,18 @@ export default class Radio extends React.Component {
             const inline = this.props.inline ? '-inline' : '';
 
             radiosToRender.push(
-                <div className={`chi-form__item ${inline}`}>
+                <div className={`chi-form__item ${inline}`} key={`radio-${i}`}>
                   <div className="chi-radio">
                     <input className="chi-radio__input" type="radio" name={`radios-${uuid}`} id={uuid}
-                       checked={i === this.props.selectedOption} disabled={this.props[`disabled${i}`]}/>
-                    <label className="chi-radio__label" htmlFor={uuid}>{this.props[`option${i}`]}</label>
+                           checked={i === this.props.selectedOption} disabled={this.props[`disabled${i}`]}
+                           onChange={() => {
+                           }}/>
+                    <label onClick={(e) => {
+                      selectRadio(e.target, uuid);
+                      if (this.props[`select${i}`]) {
+                        this.props[`select${i}`]();
+                      }
+                    }} className="chi-radio__label" htmlFor={uuid}>{this.props[`option${i}`]}</label>
                   </div>
                 </div>
             );
@@ -29,10 +49,10 @@ export default class Radio extends React.Component {
         });
     const fieldLabel = this.props.fieldLabel ? <div className="chi-label">{this.props.fieldLabel}</div> : '';
     return (
-        <div>
+        <fieldset>
           {fieldLabel}
           {radiosToRender}
-        </div>
+        </fieldset>
     );
   }
 }
@@ -43,24 +63,34 @@ Radio.propTypes = {
   selectedOption: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
   option1: PropTypes.string,
   disabled1: PropTypes.bool,
+  select1: PropTypes.func,
   option2: PropTypes.string,
   disabled2: PropTypes.bool,
+  select2: PropTypes.func,
   option3: PropTypes.string,
   disabled3: PropTypes.bool,
+  select3: PropTypes.func,
   option4: PropTypes.string,
   disabled4: PropTypes.bool,
+  select4: PropTypes.func,
   option5: PropTypes.string,
   disabled5: PropTypes.bool,
+  select5: PropTypes.func,
   option6: PropTypes.string,
   disabled6: PropTypes.bool,
+  select6: PropTypes.func,
   option7: PropTypes.string,
   disabled7: PropTypes.bool,
+  select7: PropTypes.func,
   option8: PropTypes.string,
   disabled8: PropTypes.bool,
+  select8: PropTypes.func,
   option9: PropTypes.string,
   disabled9: PropTypes.bool,
+  select9: PropTypes.func,
   option10: PropTypes.string,
   disabled10: PropTypes.bool,
+  select10: PropTypes.func,
 };
 /* eslint-enable */
 
@@ -69,5 +99,4 @@ Radio.defaultProps = {
   option1: 'Option 1',
   option2: 'Option 2',
   option3: 'Option 3',
-  selectedOption: 'None',
 };
