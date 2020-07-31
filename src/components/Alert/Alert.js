49a148d8c80cea0f60d230660caff0919d0a3e8c
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-07-31 12:28:59 +0200

    Improved Alert component

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,42 +1,64 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 
+/* eslint-disable */
 export default class Alert extends React.Component {
   render() {
-    const closeButton = (
-      <button type="button" className="chi-alert__close-button chi-button -icon -close" aria-label="Close">
-        <div className="chi-btn__content">
-          <i className="chi-icon icon-x"></i>
-        </div>
-      </button>
-    );
-    const iconToRender = this.props.icon ? this.props.icon : 'circle-check';
+    let iconToRender;
+
+    if (this.props.icon) {
+      iconToRender = this.props.icon;
+    } else {
+      switch(this.props.state) {
+        case 'success':
+          iconToRender = 'circle-check';
+        case 'warning':
+          iconToRender = 'warning';
+        case 'danger':
+          iconToRender = 'x-alt';
+        default:
+          iconToRender = 'circle-info';
+      }
+    }
+    let alertTitle = '';
+    if (this.props.title) {
+      alertTitle = <p class="chi-alert__title">{this.props.title}</p>;
+    }
+    let closeButton = '';
+    if (this.props.closable) {
+      closeButton = <button class="chi-alert__close-button chi-button -icon -close" aria-label="Close">
+      <div class="chi-button__content">
+        <i class="chi-icon icon-x"></i>
+      </div>
+    </button>;
+    }
 
     return (
-      <div className={`chi-alert
-      ${this.props.state ? `-${this.props.state}` : ''}
-      ${this.props.size ? `-${this.props.size}` : ''}
-      ${this.props.type ? `-${this.props.type}` : ''}
-    `}>
-        <i className={`chi-alert__icon chi-icon icon-${iconToRender}`}></i>
-        <div className="chi-alert__content">
-          {this.props.title ? <p className="chi-alert__title">{this.props.title}</p> : ''}
-          <p className="chi-alert__text">{this.props.text}</p>
+      <div class={`
+        chi-alert
+        ${this.props.state ? `-${this.props.state}` : ''}
+        ${this.props.size ? `-${this.props.size}` : ''}
+        ${this.props.type ? `-${this.props.type}` : ''}
+        `}
+        role="alert">
+        <i class={`chi-alert__icon chi-icon icon-${iconToRender}`}></i>
+        <div class="chi-alert__content">
+          {alertTitle}
+          <p class="chi-alert__text">{this.props.text}</p>
         </div>
-        {this.props.closable ? closeButton : ''}
+        {closeButton}
       </div>
     );
   }
 }
 
-/* eslint-disable */
 Alert.propTypes = {
   size: PropTypes.oneOf(['sm', 'md', 'lg']),
   state: PropTypes.oneOf(['success', 'warning', 'danger', 'info', 'muted']),
   text: PropTypes.string,
   title: PropTypes.string,
   icon: PropTypes.string,
-  type: PropTypes.oneOf(['banner', 'toast']),
+  type: PropTypes.oneOf(['bubble', 'banner', 'toast']),
   closable: PropTypes.bool,
 };
 /* eslint-enable */
@@ -44,5 +66,6 @@ Alert.propTypes = {
 Alert.defaultProps = {
   size: 'md',
   state: '',
+  type: 'bubble',
   text: 'This is an alert component',
 };
