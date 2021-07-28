b56ab49430ec41ab67376cafec60008f512c6422
Riya  Abraham <69689245+riya-abraham01@users.noreply.github.com>
2021-07-28 10:00:38 -0500

    Update src/components/Floating-button/Floating-button.js
    Co-authored-by: Jani Bolkvadze <jani.bolkvadze@gmail.com>

--- src/components/Floating-button/Floating-button.js
+++ src/components/Floating-button/Floating-button.js
@@ -5,7 +5,6 @@ import * as React from 'react';
 
 export default class FloatingButton extends React.Component {
   render() {
-    
     return (
       <chi-button type="float" color={this.props.color} alternative-text="Button action">
         <chi-icon icon={this.props.icon}></chi-icon>
