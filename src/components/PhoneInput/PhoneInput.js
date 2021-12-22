81891ff14c5364dc3d2696e7d97c2225847aa822
josecarlosgonzalezv <josecarlos.gonzalezvillegas@cgi.com>
2021-12-22 13:26:04 +0100

    [FSDE-12034] Added a function to map countries without abbreviations

--- src/components/PhoneInput/PhoneInput.js
+++ src/components/PhoneInput/PhoneInput.js
@@ -15,6 +15,33 @@ export default class PhoneInput extends React.Component {
     this.state = { id: uuid4() };
   }
 
+  mapCountry(country) {
+    switch (country) {
+      case 'United States':
+        return 'US';
+      case 'Brazil':
+        return 'BR';
+      case 'China':
+        return 'CN';
+      case 'France':
+        return 'FR';
+      case 'Germany':
+        return 'DE';
+      case 'India':
+        return 'IN';
+      case 'Italy':
+        return 'IT';
+      case 'Russia':
+        return 'RU';
+      case 'Spain':
+        return 'ES';
+      case 'United Kingdom':
+        return 'GB';
+      default:
+        return 'United States';
+    }
+  }
+
   render() {
     this.key += 1;
 
@@ -26,7 +53,7 @@ export default class PhoneInput extends React.Component {
         }}>
         <chi-label for={`phone-input-${this.state.id}`}>{this.props.label}</chi-label>
         <chi-phone-input
-          default-country={this.props.country}
+          default-country={this.mapCountry(this.props.country)}
           disabled={this.props.disabled}
           id={`phone-input-${this.state.id}`}
           placeholder={this.props.placeholder}
@@ -46,29 +73,7 @@ export default class PhoneInput extends React.Component {
 
 PhoneInput.propTypes = {
   disabled: PropTypes.bool,
-  country: PropTypes.oneOf([
-    'AC', 'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AR', 'AS', 'AT',
-    'AU', 'AW', 'AX', 'AZ', 'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI',
-    'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS', 'BT', 'BW', 'BY', 'BZ',
-    'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO',
-    'CR', 'CU', 'CV', 'CW', 'CX', 'CY', 'CZ', 'DE', 'DJ', 'DK', 'DM', 'DO',
-    'DZ', 'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET', 'FI', 'FJ', 'FK', 'FM',
-    'FO', 'FR', 'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM',
-    'GN', 'GP', 'GQ', 'GR', 'GT', 'GU', 'GW', 'GY', 'HK', 'HN', 'HR', 'HT',
-    'HU', 'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS', 'IT', 'JE',
-    'JM', 'JO', 'JP', 'KE', 'KG', 'KH', 'KI', 'KM', 'KN', 'KP', 'KR', 'KW',
-    'KY', 'KZ', 'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV',
-    'LY', 'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN',
-    'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ',
-    'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ',
-    'OM', 'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PR', 'PS', 'PT',
-    'PW', 'PY', 'QA', 'RE', 'RO', 'RS', 'RU', 'RW', 'SA', 'SB', 'SC', 'SD',
-    'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS',
-    'ST', 'SV', 'SX', 'SY', 'SZ', 'TA', 'TC', 'TD', 'TG', 'TH', 'TJ', 'TK',
-    'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ', 'UA', 'UG', 'US',
-    'UY', 'UZ', 'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU', 'WF', 'WS', 'XK',
-    'YE', 'YT', 'ZA', 'ZM', 'ZW',
-  ]),
+  country: PropTypes.oneOf(['United States', 'Brazil', 'China', 'France', 'Germany', 'India', 'Italy', 'Russia', 'Spain', 'United Kingdom']),
   label: PropTypes.string,
   placeholder: PropTypes.string,
   size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
@@ -79,7 +84,7 @@ PhoneInput.propTypes = {
 };
 
 PhoneInput.defaultProps = {
-  country: 'US',
+  country: 'United States',
   label: 'Phone Number',
   size: 'md',
   width: 300,
