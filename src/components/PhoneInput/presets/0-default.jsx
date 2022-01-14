a2be9b1f9defcb49f1adcc850d5e1eb9f4d4072c
José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>
2022-01-14 17:28:04 +0100

    [FSDE-12034] Add support of Phone Input component (#79)
    * [FSDE-12034] Add support of Phone Input component
    
    * [FSDE-12034] Added a function to map countries without abbreviations
    
    * [FSDE-12034] Update default country value
    
    * [FSDE-12034] Fixed container box, added required options, added iteractions and changed defaults
    
    * [FSDE-12034] Removed default width and label class

---
+++ src/components/PhoneInput/presets/0-default.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PhoneInput from '../PhoneInput';
+
+export default (
+  <PhoneInput
+    uxpId="phoneInput"
+    country="United States"
+    click={() => {}}
+    info={false}
+    label="Phone Number"
+    mouseLeave={() => {}}
+    mouseOver={() => {}}
+    input={() => {}}
+    required="none"
+    size="md"
+    valueChange={() => {}}
+  />
+);
