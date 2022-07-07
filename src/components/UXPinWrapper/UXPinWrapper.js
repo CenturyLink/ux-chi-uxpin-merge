c21941c051c556effeaf0c4348a506f64225d85b
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2022-07-07 16:34:25 +0200

    [FSDE-23396] Update Chi to 5.15.0 (#98)

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -1,26 +1,26 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './UXPinWrapper.css';
+import { CHI_VERSION } from '../../constants/constants';
 
 export default function UXPinWrapper({ children }) {
   if (!document.getElementById('chi-css')) {
     const chiCss = document.createElement('link');
     chiCss.setAttribute('rel', 'stylesheet');
-    chiCss.setAttribute('href', 'https://assets.ctl.io/chi/5.14.0/chi-portal.css');
+    chiCss.setAttribute('href', `https://assets.ctl.io/chi/${CHI_VERSION}/chi-portal.css`);
     chiCss.setAttribute('id', 'chi-css');
     document.head.appendChild(chiCss);
   }
 
   if (!document.getElementById('chi-js')) {
     const chiJs = document.createElement('script');
-    chiJs.setAttribute('src', 'https://assets.ctl.io/chi/5.14.0/js/chi.js');
+    chiJs.setAttribute('src', `https://assets.ctl.io/chi/${CHI_VERSION}/js/chi.js`);
     chiJs.setAttribute('id', 'chi-js');
     document.head.appendChild(chiJs);
   }
 
   if (!document.getElementById('chi-ce-module')) {
     const chiCeModule = document.createElement('script');
-    chiCeModule.setAttribute('src', 'https://assets.ctl.io/chi/5.14.0/js/ce/ux-chi-ce/ux-chi-ce.esm.js');
+    chiCeModule.setAttribute('src', `https://assets.ctl.io/chi/${CHI_VERSION}/js/ce/ux-chi-ce/ux-chi-ce.esm.js`);
     chiCeModule.setAttribute('type', 'module');
     chiCeModule.setAttribute('id', 'chi-ce-module');
     document.head.appendChild(chiCeModule);
@@ -28,17 +28,15 @@ export default function UXPinWrapper({ children }) {
 
   if (!document.getElementById('chi-ce-nomodule')) {
     const chiCeNomodule = document.createElement('script');
-    chiCeNomodule.setAttribute('src', 'https://assets.ctl.io/chi/5.14.0/js/ce/ux-chi-ce/ux-chi-ce.js');
+    chiCeNomodule.setAttribute('src', `https://assets.ctl.io/chi/${CHI_VERSION}/js/ce/ux-chi-ce/ux-chi-ce.js`);
     chiCeNomodule.setAttribute('nomodule', '');
     chiCeNomodule.setAttribute('id', 'chi-ce-nomodule');
     document.head.appendChild(chiCeNomodule);
   }
 
-  /* eslint-disable */
   return (
-    <div className="chi" style={{resize: 'both'}}>
+    <div className="chi" style={{ resize: 'both' }}>
       {children}
     </div>
   );
-  /* eslint-enable */
 }
