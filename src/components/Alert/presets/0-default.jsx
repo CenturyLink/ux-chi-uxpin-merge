3a004ad804cabeba27fb736bfcb386f20d5ea984
soujanreddy <soujan.reddy@lumen.com>
2022-11-23 07:42:12 -0700

    FSDE-29965-Refactor to add anchor elements for clickable alert

--- src/components/Alert/presets/0-default.jsx
+++ src/components/Alert/presets/0-default.jsx
@@ -11,5 +11,6 @@ export default (
     icon=""
     type="bubble"
     closable={false}
+    click={() => {}}
   />
 );
--- src/constants/classes.js
+++ src/constants/classes.js
@@ -14,6 +14,8 @@ export const PORTAL_CLASS = '-portal';
 export const TRANSITIONING_CLASS = '-transitioning';
 export const SR_ONLY_CLASS = '-sr--only';
 export const UX_PIN_WRAPPER = 'uxPin__wrapper';
+export const NO_HOVER_UNDERLINE = '-no-hover-underline';
+export const ICON_CHEVRON_RIGHT = 'icon-chevron-right';
 export const GENERIC_SIZES = {
   XS: '-xs',
   SM: '-sm',
@@ -127,6 +129,13 @@ export const UTILITY_CLASSES = {
   },
   DISPLAY: {
     FLEX: '-d--flex',
+  },
+  TYPOGRAPHY: {
+    TEXT_PRIMARY: '-text--primary',
+    TEXT_TRUNCATE: '-text--truncate',
+    TEXT_NORMAL: '-text--normal',
+    TEXT_SEMI_BOLD: '-text--semi-bold',
+    TEXT_BOLD: '-text--bold'
   },
   GRID: 'chi-grid',
   COLUMN: 'chi-col',
@@ -286,6 +295,11 @@ export const STEPS_CLASSES = {
 /* Spinner */
 export const SPINNER_CLASSES = {
   SPINNER: 'chi-spinner',
+}
+
+/* Link */
+export const LINK_CLASSES = {
+  LINK: 'chi-link',
 }
 
 /* eslint-enable */
