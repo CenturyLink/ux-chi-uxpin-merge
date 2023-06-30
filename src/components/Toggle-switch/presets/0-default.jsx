c7f0fbd02d17779ec9ec96bc871282ccce94c310
soujanreddy <soujan.reddy@lumen.com>
2023-06-30 15:38:33 -0600

    [FSDE-31545] Add support for sizes in Toggle switch in UXPin

--- src/components/Toggle-switch/presets/0-default.jsx
+++ src/components/Toggle-switch/presets/0-default.jsx
@@ -4,6 +4,12 @@ import ToggleSwitch from '../Toggle-switch';
 export default (
   <ToggleSwitch
     uxpId="toggle-switch"
+    size="xs"
     label="Label"
+    toggleLabel="Toggle Label"
+    on={false}
+    required="none"
+    disabled={false}
+    info={false}
   />
 );
