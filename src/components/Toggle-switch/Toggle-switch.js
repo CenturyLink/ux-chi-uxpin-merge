988186ddd86ff2c343cef81fd4f38c5508ae445e
soujanreddy <soujan.reddy@lumen.com>
2023-06-30 15:38:33 -0600

    [FSDE-31545] Add support for sizes in Toggle switch in UXPin

--- src/components/Toggle-switch/Toggle-switch.js
+++ src/components/Toggle-switch/Toggle-switch.js
@@ -1,49 +1,104 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
+import Label from '../Label/Label';
+import Icon from '../Icon/Icon';
+import { uuid4 } from '../../utils/utils';
+import { LABEL_CLASSES, STAT_CLASSES } from '../../constants/classes';
 
 /* eslint-disable */
 /**
  * @uxpincomponent
  */
-const ToggleSwitch = (props) => (
-  <div className="chi-form__item"
-       onMouseEnter={props.mouseOver}
-       onMouseLeave={props.mouseLeave}
-       onMouseDown={props.mouseDown}
-       onMouseUp={props.mouseUp}>
-    <label className="chi-switch">
-      <input
-          type="checkbox"
-          className="chi-switch__input"
-          disabled={props.disabled}
-          onChange={(e) => {
-            if (e.target.checked) {
-              props.turnOn();
-            } else {
-              props.turnOff();
-            }
-          }}
-          checked={props.on} />
-      <span className="chi-switch__content">
-        <span className="chi-switch__thumb"></span>
-      </span>
-      {props.label ? <span className="chi-switch__label">{props.label}</span> : ''}
-    </label>
-  </div>
-);
+export default class ToggleSwitch extends React.Component {
+  key = 0;
+
+  constructor(props) {
+    super(props);
+    this.state = { id: uuid4() };
+  }
+
+  render() {
+    const info = this.props.info
+    ? (
+      <div className={`${STAT_CLASSES.TITLE_HELP}`}>
+        <Icon
+          uxpId={`infoIcon-${this.state.id}`}
+          icon={'circle-info-outline'}
+          size="xs"
+          color="primary"
+          mode="button"
+          popover={true}
+          popoverTitle={this.props.infoPopoverTitle}
+          popoverDescription={this.props.infoPopoverDescription}
+          popoverPosition={this.props.infoPopoverPosition}
+        />
+      </div>
+    ) : '';
+
+    const label = this.props.label
+    ? (
+      <Label
+        className="chi-label"
+        htmlFor={this.state.id}
+        required={this.props.required}
+        label={this.props.label}>
+      </Label>
+    )
+    : null;
+
+    return (
+      <div
+        key={this.key}
+        ref={this.props.uxpinRef}>
+        <div className={`${LABEL_CLASSES.WRAPPER}`}>
+          {label}
+          {info}
+        </div>
+        <chi-switch
+          id={this.state.id}
+          label={this.props.toggleLabel}
+          size={this.props.size}
+          checked={this.props.on}
+          disabled={this.props.disabled}
+        >
+        </chi-switch>
+      </div>
+    );
+  }
+}
 
 ToggleSwitch.propTypes = {
   label: PropTypes.string,
+  toggleLabel: PropTypes.string,
   on: PropTypes.bool,
   disabled: PropTypes.bool,
+  size: PropTypes.oneOf(['xs', 'md']),
+  required: PropTypes.oneOf(['none', 'required', 'optional']),
+  info: PropTypes.bool,
+  infoPopoverTitle: PropTypes.string,
+  /**
+   * A textArea controller for Text
+   * @uxpinpropname text
+   * @uxpincontroltype textfield(10)
+   * */
+  infoPopoverDescription: PropTypes.string,
+  infoPopoverPosition: PropTypes.oneOf(['right-start', 'top']),
   turnOn: PropTypes.func,
   turnOff: PropTypes.func,
 };
 /* eslint-enable */
 
 ToggleSwitch.defaultProps = {
-  disabled: false,
   label: 'Label',
+  toggleLabel: 'Toggle Label',
+  on: false,
+  disabled: false,
+  size: 'xs',
+  required: 'none',
+  info: false,
+  infoPopoverTitle: 'Popover Title',
+  infoPopoverDescription: `Line 1
+  Line 2
+  Line 3`,
+  infoPopoverPosition: 'right-start',
 };
-
-export { ToggleSwitch as default };
