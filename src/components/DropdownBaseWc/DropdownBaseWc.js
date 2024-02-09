8a69af4e8c9309aba739ce2d982bda5deaa0de2c
soujanreddy <soujan.reddy@lumen.com>
2024-02-08 21:43:32 -0700

    [DPEDE-1203] Create Dropdown base web component in UXPin

---
+++ src/components/DropdownBaseWc/DropdownBaseWc.js
@@ -0,0 +1,217 @@
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import DropdownMenuItems from '../DropdownMenuItems/DropdownMenuItems';
+import Label from '../Label/Label';
+import { DROPDOWN_CLASSES, LABEL_CLASSES } from '../../constants/classes';
+
+/**
+ * @uxpincomponent
+ */
+export default function DropdownBaseWc(props) {
+  const {
+    active,
+    animateChevron,
+    buttonColor,
+    buttonSize,
+    buttonType,
+    disabled,
+    dropdownPosition,
+    fieldLabel,
+    info,
+    infoPopoverTitle,
+    infoPopoverDescription,
+    infoPopoverPosition,
+    mode,
+    required,
+    retainSelection,
+    selectedItem,
+    syncTextWithSelectedItem,
+    text,
+  } = props;
+
+  // #region Methods
+
+  const [buttonText, setButtonText] = useState(text);
+  const dropdownRef = useRef(null);
+
+  const createDropdownItems = () => {
+    const items = [];
+    for (let i = 1; i <= 10; i++) {
+      const title = props[`item${i}`];
+      if (title) {
+        items.push({
+          title,
+          iconLeft: props[`iconLeft${i}`],
+          iconRight: props[`iconRight${i}`],
+          description: props[`item${i}Description`],
+          onSelect: () => handleSelect(i),
+        });
+      }
+    }
+    return items;
+  };
+
+  const items = createDropdownItems();
+
+  const handleSelect = (index) => {
+    const item = items[index - 1];
+    if (item) {
+      if (syncTextWithSelectedItem && ['base', 'radio'].includes(mode)) {
+        setButtonText(item.title);
+      }
+      if (mode === 'base' && dropdownRef.current) {
+        dropdownRef.current.hide();
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!syncTextWithSelectedItem) {
+      setButtonText(text);
+    }
+  }, [text, syncTextWithSelectedItem]);
+
+  const hasDescription = items.some((item) => item.description);
+
+  const labelElement = fieldLabel && (
+    <Label
+      required={required}
+      label={fieldLabel}
+      info={info}
+      infoPopoverTitle={infoPopoverTitle}
+      infoPopoverDescription={infoPopoverDescription}
+      infoPopoverPosition={infoPopoverPosition}
+    />
+  );
+  // #endregion
+
+  return (
+    <div className={DROPDOWN_CLASSES.DROPDOWN}>
+      <div className={LABEL_CLASSES.WRAPPER}>{labelElement}</div>
+      <chi-dropdown
+        id={`dropdown-base-${active ? 'active' : 'default'}`}
+        ref={dropdownRef}
+        active={active}
+        description={hasDescription ? true : undefined}
+        animate-chevron={animateChevron}
+        button={buttonText}
+        color={buttonColor}
+        disabled={disabled}
+        position={dropdownPosition}
+        size={buttonSize}
+        variant={buttonType}
+      >
+        <DropdownMenuItems
+          items={items}
+          mode={mode}
+          selectedItem={selectedItem}
+          retainSelection={retainSelection}
+          onSelect={(index) => handleSelect(index)}
+        />
+      </chi-dropdown>
+    </div>
+  );
+}
+
+// #region PropTypes
+DropdownBaseWc.propTypes = {
+  fieldLabel: PropTypes.string,
+  active: PropTypes.bool,
+  animateChevron: PropTypes.bool,
+  retainSelection: PropTypes.bool,
+  text: PropTypes.string,
+  mode: PropTypes.oneOf(['base', 'checkbox', 'radio']),
+  syncTextWithSelectedItem: PropTypes.bool,
+  buttonColor: PropTypes.oneOf(['base', 'primary', 'secondary', 'danger', 'dark', 'light']),
+  buttonType: PropTypes.oneOf(['solid', 'outline', 'flat']),
+  buttonSize: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  disabled: PropTypes.bool,
+  appSwitcher: PropTypes.bool,
+  required: PropTypes.oneOf(['none', 'required', 'optional']),
+  info: PropTypes.bool,
+  infoPopoverTitle: PropTypes.string,
+  /**
+   * @uxpinpropname text
+   * @uxpincontroltype textfield(10)
+   * */
+  infoPopoverDescription: PropTypes.string,
+  infoPopoverPosition: PropTypes.oneOf(['right-start', 'top']),
+  dropdownHeight: PropTypes.string,
+  dropdownFixedWidth: PropTypes.string,
+  dropdownPosition: PropTypes.oneOf([
+    'top-start',
+    'top',
+    'top-end',
+    'left-start',
+    'left',
+    'left-end',
+    'right-start',
+    'right',
+    'right-end',
+    'bottom-start',
+    'bottom',
+    'bottom-end',
+  ]),
+  selectedItem: PropTypes.oneOf(['None', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+  item1: PropTypes.string,
+  item2: PropTypes.string,
+  item3: PropTypes.string,
+  item4: PropTypes.string,
+  item5: PropTypes.string,
+  item6: PropTypes.string,
+  item7: PropTypes.string,
+  item8: PropTypes.string,
+  item9: PropTypes.string,
+  item10: PropTypes.string,
+  iconLeft1: PropTypes.string,
+  iconRight1: PropTypes.string,
+  iconLeft2: PropTypes.string,
+  iconRight2: PropTypes.string,
+  iconLeft3: PropTypes.string,
+  iconRight3: PropTypes.string,
+  iconLeft4: PropTypes.string,
+  iconRight4: PropTypes.string,
+  iconLeft5: PropTypes.string,
+  iconRight5: PropTypes.string,
+  iconLeft6: PropTypes.string,
+  iconRight6: PropTypes.string,
+  iconLeft7: PropTypes.string,
+  iconRight7: PropTypes.string,
+  iconLeft8: PropTypes.string,
+  iconRight8: PropTypes.string,
+  iconLeft9: PropTypes.string,
+  iconRight9: PropTypes.string,
+  iconLeft10: PropTypes.string,
+  iconRight10: PropTypes.string,
+  item1Description: PropTypes.string,
+  item2Description: PropTypes.string,
+  item3Description: PropTypes.string,
+  item4Description: PropTypes.string,
+  item5Description: PropTypes.string,
+  item6Description: PropTypes.string,
+  item7Description: PropTypes.string,
+  item8Description: PropTypes.string,
+  item9Description: PropTypes.string,
+  item10Description: PropTypes.string,
+  buttonClick: PropTypes.func,
+  select1: PropTypes.func,
+  select2: PropTypes.func,
+  select3: PropTypes.func,
+  select4: PropTypes.func,
+  select5: PropTypes.func,
+  select6: PropTypes.func,
+  select7: PropTypes.func,
+  select8: PropTypes.func,
+  select9: PropTypes.func,
+  select10: PropTypes.func,
+};
+
+DropdownBaseWc.defaultProps = {
+  active: false,
+  mode: 'base',
+  syncTextWithSelectedItem: false,
+  selectedItem: 1,
+  retainSelection: false,
+  text: 'Dropdown component',
+};
+// #endregion
