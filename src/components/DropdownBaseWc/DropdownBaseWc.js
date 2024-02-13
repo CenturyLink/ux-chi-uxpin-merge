b09a2bed73655a28f71d8441645dd2532ff789a9
soujanreddy <soujan.reddy@lumen.com>
2024-02-12 17:29:00 -0700

    Updates notes

--- src/components/DropdownBaseWc/DropdownBaseWc.js
+++ src/components/DropdownBaseWc/DropdownBaseWc.js
@@ -66,6 +66,7 @@ export default function DropdownBaseWc(props) {
   // TODO: Implement custom dropdown fixed width and height for dropdown on chi
   // TODO: Retain selection with active selected items should be implemented on chi
   // TODO: Selected dropdown title item should update/sync dropdown text (from chi)
+  // TODO: appSwitcher property is appending -text--xl -px--1 classes in previous implementation
   return (
     <>
       <div className={LABEL_CLASSES.WRAPPER}>{labelElement}</div>
