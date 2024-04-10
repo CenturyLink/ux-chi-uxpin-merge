3a155ffb09a981d54e0e5e4611e1c99bc394eef1
Wojciech Wlodarczyk <wojciech.wlodarczyk@lumen.com>
2024-04-10 16:58:50 +0200

    [DPEDE-1677] Deleted extra space from label wrapper in dropdowns, run prettier

--- src/components/DropdownBase/DropdownBase.js
+++ src/components/DropdownBase/DropdownBase.js
@@ -53,25 +53,23 @@ export default class DropdownBase extends React.Component {
 
   render() {
     const label = this.props.fieldLabel ? (
-      <Label
-        htmlFor={this.state.id}
-        required={this.props.required}
-        label={this.props.fieldLabel}
-        info={this.props.info}
-        infoPopoverTitle={this.props.infoPopoverTitle}
-        infoPopoverDescription={this.props.infoPopoverDescription}
-        infoPopoverPosition={this.props.infoPopoverPosition}>
-      </Label>
+      <div className={LABEL_CLASSES.WRAPPER}>
+        <Label
+          htmlFor={this.state.id}
+          required={this.props.required}
+          label={this.props.fieldLabel}
+          info={this.props.info}
+          infoPopoverTitle={this.props.infoPopoverTitle}
+          infoPopoverDescription={this.props.infoPopoverDescription}
+          infoPopoverPosition={this.props.infoPopoverPosition}
+        ></Label>
+      </div>
     ) : null;
 
     return (
       // eslint-disable-next-line react/prop-types
-      <div
-        className={`${DROPDOWN_CLASSES.DROPDOWN} ${UTILITY_CLASSES.WIDTH[100]}`}
-        ref={this.props.uxpinRef}>
-        <div className={LABEL_CLASSES.WRAPPER}>
-          {label}
-        </div>
+      <div className={`${DROPDOWN_CLASSES.DROPDOWN} ${UTILITY_CLASSES.WIDTH[100]}`} ref={this.props.uxpinRef}>
+        {label}
         <chi-button
           ref={this.dropdownBaseRef}
           id={this.state.id}
@@ -89,8 +87,11 @@ export default class DropdownBase extends React.Component {
           color={this.props.buttonColor === 'base' ? '' : `${this.props.buttonColor}`}
           variant={this.props.buttonType === 'solid' ? '' : `${this.props.buttonType}`}
           data-position={this.props.dropdownPosition}
-          onClick={() => this.handleButtonClick()}>
-          <span className={`${OVERFLOW_HIDDEN} ${UTILITY_CLASSES.TYPOGRAPHY.TEXT_TRUNCATE} ${UTILITY_CLASSES.WIDTH[100]} ${UTILITY_CLASSES.TEXT.LEFT}`}>
+          onClick={() => this.handleButtonClick()}
+        >
+          <span
+            className={`${OVERFLOW_HIDDEN} ${UTILITY_CLASSES.TYPOGRAPHY.TEXT_TRUNCATE} ${UTILITY_CLASSES.WIDTH[100]} ${UTILITY_CLASSES.TEXT.LEFT}`}
+          >
             {this.state.text || this.props.text}
           </span>
         </chi-button>
@@ -152,8 +153,8 @@ export default class DropdownBase extends React.Component {
           iconLeft7={this.props.iconLeft7}
           iconLeft8={this.props.iconLeft8}
           iconLeft9={this.props.iconLeft9}
-          iconLeft10={this.props.iconLeft10}>
-        </DropdownMenu>
+          iconLeft10={this.props.iconLeft10}
+        ></DropdownMenu>
       </div>
     );
   }
@@ -183,7 +184,20 @@ DropdownBase.propTypes = {
    * */
   infoPopoverDescription: PropTypes.string,
   infoPopoverPosition: PropTypes.oneOf(['right-start', 'top']),
-  dropdownPosition: PropTypes.oneOf(['top-start', 'top', 'top-end', 'left-start', 'left', 'left-end', 'right-start', 'right', 'right-end', 'bottom-start', 'bottom', 'bottom-end']),
+  dropdownPosition: PropTypes.oneOf([
+    'top-start',
+    'top',
+    'top-end',
+    'left-start',
+    'left',
+    'left-end',
+    'right-start',
+    'right',
+    'right-end',
+    'bottom-start',
+    'bottom',
+    'bottom-end',
+  ]),
   dropdownHeight: PropTypes.string,
   dropdownFixedWidth: PropTypes.string,
   selectedItem: PropTypes.oneOf(['None', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
