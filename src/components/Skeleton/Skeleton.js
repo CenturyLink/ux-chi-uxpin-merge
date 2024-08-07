4fe062b30a7c1fd4e61af39c48d0879d4595d044
wojciechwlodarczyklumen <139760197+wojciechwlodarczyklumen@users.noreply.github.com>
2024-08-07 17:04:05 +0200

    [DPEDE-2332] Added skeleton visual tests and height prop (#257)
    * [DPEDE-2332] Added skeleton visual tests, added possiblity to set different skeleton height
    
    * [DPEDE-2332] Improved custom sizing of skeleton components
    
    * [DPEDE-2332] Improved code, refactor
    
    * [DPEDE-2332] Changed condition

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -20,28 +20,46 @@ const skeletonType = (type) => {
   return '';
 };
 
-function Skeleton({ size, type, uxpinRef }) {
+const getStyles = (size, type, customSize) => {
+  if (size !== 'custom') return {};
+
+  const typesWithWidth = ['circle', 'rounded squared', 'square'];
+
+  return {
+    height: `${customSize}px`,
+    width: typesWithWidth.includes(type) ? `${customSize}px` : undefined,
+    borderRadius: type === 'circle' ? '50%' : undefined,
+  };
+};
+
+function Skeleton({ size, type, customSize, uxpinRef }) {
   return (
     <div ref={uxpinRef}>
       <div
+        style={getStyles(size, type, customSize)}
         className={`
     ${SKELETON_CLASSES.SKELETON} 
     ${skeletonType(type)}
     ${size ? `-${size}` : ''}
-  `}>
-      </div>
+  `}
+      ></div>
     </div>
   );
 }
 
 Skeleton.propTypes = {
-  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl', 'custom']),
   type: PropTypes.oneOf(['circle', 'rounded', 'rounded squared', 'square']),
+  /**
+   * @uxpinpropname Custom Size
+   * */
+  customSize: PropTypes.number,
 };
 
 Skeleton.defaultProps = {
   size: 'md',
   type: 'default',
+  customSize: 50,
 };
 
 export default Skeleton;
