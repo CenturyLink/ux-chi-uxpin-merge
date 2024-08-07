acc2dbd88d7b3a72b3fb401b40c50ec49f64230f
Wojciech Wlodarczyk <wojciech.wlodarczyk@lumen.com>
2024-08-07 14:27:21 +0200

    [DPEDE-2332] Added skeleton visual tests, added possiblity to set different skeleton height

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -20,16 +20,46 @@ const skeletonType = (type) => {
   return '';
 };
 
-function Skeleton({ size, type, uxpinRef }) {
+const getStyles = () => {
+  switch (type) {
+    case 'circle':
+      return {
+        height: `${height}px`,
+        width: `${height}px`,
+        borderRadius: '50%',
+      };
+    case 'rounded':
+      return {
+        height: `${height}px`,
+      };
+    case 'rounded squared':
+      return {
+        height: `${height}px`,
+        width: `${height}px`,
+      };
+    case 'square':
+      return {
+        height: `${height}px`,
+        width: `${height}px`,
+      };
+    default:
+      return {
+        height: `${height}px`,
+      };
+  }
+};
+
+function Skeleton({ size, type, height, uxpinRef }) {
   return (
     <div ref={uxpinRef}>
       <div
+        style={getStyles()}
         className={`
     ${SKELETON_CLASSES.SKELETON} 
     ${skeletonType(type)}
     ${size ? `-${size}` : ''}
-  `}>
-      </div>
+  `}
+      ></div>
     </div>
   );
 }
@@ -37,6 +67,7 @@ function Skeleton({ size, type, uxpinRef }) {
 Skeleton.propTypes = {
   size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
   type: PropTypes.oneOf(['circle', 'rounded', 'rounded squared', 'square']),
+  height: PropTypes.number,
 };
 
 Skeleton.defaultProps = {
