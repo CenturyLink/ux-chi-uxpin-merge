84740e5c183e2c8231b63e9a024c7f3ed80dd551
josecarlosgonzalezv <josecarlos.gonzalezvillegas1@lumen.com>
2024-08-20 11:33:53 +0200

    [DPEDE-2378] Create POC to update UXPin to latest rebrand version

--- src/components/UXPinWrapper/UXPinWrapper.js
+++ src/components/UXPinWrapper/UXPinWrapper.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import './UXPinWrapper.css';
-import { CHI_VERSION } from '../../constants/constants';
+import { CHI_REBRAND_VERSION, CHI_VERSION } from '../../constants/constants';
 
 export default function UXPinWrapper({ children }) {
+  const chiVersion = process.env.THEME === 'portal' ? CHI_VERSION : CHI_REBRAND_VERSION;
+
   if (!document.getElementById('chi-css')) {
     const chiCss = document.createElement('link');
     chiCss.setAttribute('rel', 'stylesheet');
-    chiCss.setAttribute('href', `https://assets.ctl.io/chi/${CHI_VERSION}/chi-portal.css`);
+    chiCss.setAttribute('href', `https://lib.lumen.com/chi/${chiVersion}/chi-portal.css`);
     chiCss.setAttribute('id', 'chi-css');
     document.head.appendChild(chiCss);
   }
 
   if (!document.getElementById('chi-js')) {
     const chiJs = document.createElement('script');
-    chiJs.setAttribute('src', `https://assets.ctl.io/chi/${CHI_VERSION}/js/chi.js`);
+    chiJs.setAttribute('src', `https://lib.lumen.com/chi/${chiVersion}/js/chi.js`);
     chiJs.setAttribute('id', 'chi-js');
     document.head.appendChild(chiJs);
   }
 
   if (!document.getElementById('chi-ce-module')) {
     const chiCeModule = document.createElement('script');
-    chiCeModule.setAttribute('src', `https://assets.ctl.io/chi/${CHI_VERSION}/js/ce/ux-chi-ce/ux-chi-ce.esm.js`);
+    chiCeModule.setAttribute('src', `https://lib.lumen.com/chi/${chiVersion}/js/ce/ux-chi-ce/ux-chi-ce.esm.js`);
     chiCeModule.setAttribute('type', 'module');
     chiCeModule.setAttribute('id', 'chi-ce-module');
     document.head.appendChild(chiCeModule);
@@ -28,7 +30,7 @@ export default function UXPinWrapper({ children }) {
 
   if (!document.getElementById('chi-ce-nomodule')) {
     const chiCeNomodule = document.createElement('script');
-    chiCeNomodule.setAttribute('src', `https://assets.ctl.io/chi/${CHI_VERSION}/js/ce/ux-chi-ce/ux-chi-ce.js`);
+    chiCeNomodule.setAttribute('src', `https://lib.lumen.com/chi/${chiVersion}/js/ce/ux-chi-ce/ux-chi-ce.js`);
     chiCeNomodule.setAttribute('nomodule', '');
     chiCeNomodule.setAttribute('id', 'chi-ce-nomodule');
     document.head.appendChild(chiCeNomodule);
